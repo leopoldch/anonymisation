df04d76ebba165e75bf07e6a8d525f91b85e11dd
Léo <leopold@lchappuis.fr>
2026-01-18 11:38:15 -0500

    improved

--- anonymizer.py
+++ anonymizer.py
@@ -1,8 +1,47 @@
+import hashlib
+from pandas import DataFrame
+
 
 class Anonymizer:
 
-    def pseudonymization():
-        pass
+    def pseudonymization(self, df: DataFrame, key: str) -> DataFrame:
+        df[key] = df[key].map(
+            lambda pseudo: hashlib.sha256(str(pseudo).encode()).hexdigest()
+        )
+        return df
 
-    def anonymizeSensorLocations():
-        pass
+    def anonymize_sensor_locations(self, df: DataFrame, key: str) -> DataFrame:
+        sensor_locations = df[key]
+        # toutes les chambres sont composées de Ward-X, Room-Y, Bed-Z
+        # dans un premier temps on généralise en enlevant l'information des lits
+        # et on regarde combien de fois nos données sont réutilisées en moyenne
+
+        def remove_bed_from_location(location: str):
+            location_array = location.split(",")
+            return f"{location_array[0]},{location_array[1]}"
+
+        sensor_locations = sensor_locations.map(remove_bed_from_location)
+
+        df[key] = sensor_locations
+        return df
+
+    def verify_k_anonymization(self, df: DataFrame, keys: list) -> int:
+        group_sizes = df.groupby(keys).size()
+        k_value = group_sizes.min()
+        return k_value
+
+    def verify_l_anonymization(
+        self, df: DataFrame, group_key: str, sensible_data_keys: list, l: int = 3
+    ) -> bool:
+        groups = df.groupby(group_key)
+
+        for _, group_df in groups:
+            # pour tous les groupes on vérifie qu'il y ait au moins
+            # l données sensibles différentes
+
+            different_sensible_data_groups = group_df.groupby(sensible_data_keys).size()
+
+            if len(different_sensible_data_groups) < l:
+                return False
+
+        return True
--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import csv
 import numpy as np
+import pandas as pd
 from anonymizer import Anonymizer
 
 FILE_PATH = "./smart_hospital_dataset.csv"
 NEW_FILE_PATH = "./anonymized_smart_hospital_dataset.csv"
 
-myFileAsArray = []
-with open(FILE_PATH, 'r', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in reader:
-        myFileAsArray.append(row)
-
-numpy_array = np.array(myFileAsArray, dtype=str)
+df = pd.read_csv(FILE_PATH)
 
 anonymizer = Anonymizer()
 
+df = anonymizer.pseudonymization(df, "patient_identifier")
+df = anonymizer.anonymize_sensor_locations(df, "sensor_location")
 
+# En retirant le lit on obtient un k=31
+# ça semble suffisant, car si on garde que le
+# ward on se retrouve avec un k = 954 beaucoup
+# trop généralisé
+
+k = anonymizer.verify_k_anonymization(df, "sensor_location")
+print(f"K value: {k}")
+
+# La l-diversité s'occupe de cacher ce qu'on peut apprendre sur un groupe !
+# ici par exemple si tous nos patients
+# le but est de préserver la privacy
+# quelles sont les données sensibles ici ?
+# on va supposer que ce sont les données des capteurs
+# Ici on doit regarder pour chaque groupe si il y a des groupes qui présentent
+# les mêmes données sensibles
+
+# on peut choisir un l entre 3 et 5 courant et on vérifie !
+l = 5
+is_l_verification_valid = anonymizer.verify_l_anonymization(
+    df, "sensor_location", ["blood_pressure", "heart_rate"], l
+)
+
+print(f"L-diversity (L={l}) : {is_l_verification_valid}")
+
+# avec cette approche on a bien la bonne vérification mais notre algorithme ne fait
+# le rapprochement entre un patient qui a 71 et 12 de bool_pressure, il faudrait qu'on
+# un des clés moyen : .., haut : .. pour rendre plus résistant
+
+
+df.to_csv(NEW_FILE_PATH)
+
+# chiffrer les données, on peut utiliser une librairie ou pas ?
+# doit-on implémenter l'algo de chiffrement ?
+
+# sauvegarder dans la db ? Quelle db ?
